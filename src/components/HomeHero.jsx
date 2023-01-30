704651db234c8d246e645ed458c5f04972bf2fb3
gy599 <k.bajcare@gmail.com>
2023-01-30 13:15:31 +0100

    page:fix home hero page

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -1,10 +1,18 @@
-
-function HomeHero() {
+function HomeHero({ heroText, heroBtnText }) {
   return (
-    <div>
-      
+    <div className=" flex justify-between items-center bg-[url('/public/heroBG.svg')] bg-cover h-96 bg-no-repeat">
+      <div></div>
+
+      <div className="mr-24 ml-104 flex items-center flex-col  ">
+        <h2 className="font-['Lato'] font-extrabold text-5xl text-slate-50 text-center mb-12 w-[95%]">
+          {heroText}
+        </h2>
+        <button className="bg-transparent border-slate-50 rounded-none border-4 btn">
+          {heroBtnText}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomeHero
+export default HomeHero;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
+import HomeHero from "../components/HomeHero";
 
 function Home() {
   return (
     <div>
-      
+      <HomeHero
+        heroText=" stylist picks beat 
+the heat"
+        heroBtnText="shop now"
+      />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
